63ad256f5e36a38a315c853df981dea2afa2cf54
Nico Hörter <nico.m.hoerter@gmail.com>
2024-06-25 22:55:37 +0200

    starting with WinSock2.2 API

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
-#ifdef _WIN32
+#ifdef _WIN32 // WinSock
+    // winsock2.h must be included before Windows.h
+    // Windows.h is not needed in this project
     #include <winsock2.h>
-#else
+#else // POSIX
     #include <sys/socket.h>
     #include <netinet/in.h>
 #endif
 
-int main() {
+int main()
+{
+    #ifdef _WIN32
+        WSADATA wsadata;
+
+        WSAStartup(MAKEWORD(2, 2), &wsadata);
+    #endif
+
     return 0;
 }
