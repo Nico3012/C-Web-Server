674e81acf70b8febd53738a188c514a340641db9
Nico Hörter <nico.m.hoerter@gmail.com>
2024-06-27 09:16:23 +0200

    added some socket, bind, listen functions with basic error handling

--- README.md
+++ README.md
@@ -5,12 +5,13 @@
 <p>
     using <strong>cl</strong> from <strong>MSVC</strong> on <strong>Windows</strong>:<br>
     link ws2_32.lib (static libs on windows are named *.lib)<br>
-    <code>cl main.c /link ws2_32.lib</code><br>
+    run: <code>cl main.c /link ws2_32.lib</code><br>
 </p>
 
 <p>
     using <strong>gcc</strong> from <strong>MinGW</strong> on <strong>Windows</strong>:<br>
     link libws2_32.a (static libs on linux are named lib*.a)<br>
     MinGW ports the GNU Compiler Collection from Linux to Windows. Therefore, static libs are named lib*.a<br>
-    <code>gcc main.c -l:libws2_32.a</code><br>
+    The flag <code>-l ws2_32</code> includes lib*.a by it self and does the same as <code>-l:libws2_32.a</code>.<br>
+    run: <code>gcc main.c -l:libws2_32.a</code><br>
 </p>
--- main.c
+++ main.c
@@ -1,18 +1,60 @@
 #ifdef _WIN32 // WinSock
-    // winsock2.h must be included before Windows.h
-    // Windows.h is not needed in this project
+    // winsock2.h must be included before windows.h | Windows.h
+    // windows.h | Windows.h is not needed in this project
     #include <winsock2.h>
 #else // POSIX
     #include <sys/socket.h>
     #include <netinet/in.h>
 #endif
 
+#include <stdio.h>
+
 int main()
 {
     #ifdef _WIN32
-        WSADATA wsadata;
+        // Initialize WSA variables
+        WSADATA wsaData;
+        int wsaerr = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-        WSAStartup(MAKEWORD(2, 2), &wsadata);
+        // Check for initialization errors
+        if (wsaerr != 0) {
+            printf("ws2_32.dll was not found!");
+            return 1;
+        }
+
+        // Create a socket
+        SOCKET serverSocket;
+        serverSocket = INVALID_SOCKET;
+        serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        // Check for socket creation error
+        if (serverSocket == INVALID_SOCKET) {
+            printf("creating serverSocket failed!");
+            WSACleanup();
+            return 1;
+        }
+
+        // Bind the socket to an IP address and port number
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // inet_addr("127.0.0.1") | 0
+        addr.sin_port = htons(80);
+        
+        // Use bind function and check for errors
+        if (bind(serverSocket, &addr, sizeof(addr)) == SOCKET_ERROR) {
+            printf("bind() failed!");
+            return 1;
+        }
+
+        // Listen for incoming connections and check for errors
+        if (listen(serverSocket, 1) == SOCKET_ERROR) {
+            printf("listen() function failed! Error listening on socket!");
+            return 1;
+        }
+
+        // Accept incoming connections
+        SOCKET acceptSocket;
+        acceptSocket = accept(serverSocket, 0, 0);
     #endif
 
     return 0;
