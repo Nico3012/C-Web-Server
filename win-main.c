12b1d3ef0c1df7fef12731cf440e5da70bf46d61
Nico Hörter <nico.m.hoerter@gmail.com>
2024-07-01 10:08:34 +0200

    working on implementation from microsoft documentation

---
+++ win-main.c
@@ -0,0 +1,124 @@
+#include <winsock2.h>
+#include <string.h>
+#include <stdio.h>
+
+#define DEFAULT_BUFLEN 512
+
+int main()
+{
+    char *httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World!";
+
+    // initialize variables
+    int iResult;
+    WSADATA wsaData;
+    SOCKET ListenSocket;
+    SOCKET AcceptSocket;
+    int recvbuflen = DEFAULT_BUFLEN;
+    char recvbuf[DEFAULT_BUFLEN] = "";
+
+    // init Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR)
+    {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    // check Winsock version
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
+    {
+        printf("Could not find a usable version of Winsock.dll\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // create socket for listening for
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET)
+    {
+        printf("socket failed with error: %d\n", (int)WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // create service
+    struct sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr("127.0.0.1");
+    service.sin_port = htons(80);
+
+    // bind socket
+    iResult = bind(ListenSocket, (SOCKADDR *)&service, sizeof(service));
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("bind failed with error: %d\n", (int)WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // listen on socket
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("listen failed with error: %d\n", (int)WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    while (1)
+    {
+        // accept socket and store client socket
+        AcceptSocket = accept(ListenSocket, NULL, NULL);
+        if (AcceptSocket == INVALID_SOCKET)
+        {
+            printf("accept failed with error: %d\n", (int)WSAGetLastError());
+            continue;
+        }
+
+        // receive data from client and print to console
+        do
+        {
+            iResult = recv(AcceptSocket, recvbuf, recvbuflen, 0); // always stops here, since no more is sent and connection in not closed!
+            if (iResult == SOCKET_ERROR)
+            {
+                printf("recv failed with error: %d\n", (int)WSAGetLastError());
+                closesocket(AcceptSocket);
+                continue;
+            }
+            else if (iResult > 0)
+            {
+                printf("%s\n", recvbuf);
+            }
+        } while (iResult > 0);
+
+        // status log
+        printf("received everything. Now send data!\n");
+
+        // send data to client
+        iResult = send(AcceptSocket, httpResponse, (int)strlen(httpResponse), 0);
+        if (iResult == SOCKET_ERROR)
+        {
+            printf("send failed with error: %d\n", (int)WSAGetLastError());
+            closesocket(AcceptSocket);
+            continue;
+        }
+
+        // shutdown connection since no more data will be sent
+        iResult = shutdown(AcceptSocket, SD_BOTH); // shutdown send and receive
+        if (iResult == SOCKET_ERROR)
+        {
+            printf("shutdown failed with error: %d\n", (int)WSAGetLastError());
+            closesocket(AcceptSocket);
+            continue;
+        }
+
+        // close socket now after shutdown
+        // closesocket(AcceptSocket);
+    }
+
+    closesocket(ListenSocket);
+    WSACleanup();
+    return 0;
+}
--- windows.c
+++ windows.c
@@ -5,6 +5,8 @@
 
 int main()
 {
+    char *httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World!";
+
     // init Winsock 2.2:
     WORD wVersionRequested = MAKEWORD(2, 2);
     WSADATA wsaData;
@@ -21,7 +23,8 @@ int main()
         printf("Could not find a usable version of Winsock.dll\n");
 
         // stop usage of Winsock 2-DLL
-        int WSACleanupError = WSACleanup();
+        int WSACleanupError = 0;
+        WSACleanupError = WSACleanup();
         if (WSACleanupError == SOCKET_ERROR)
         {
             int WSACleanupErrorCode = WSAGetLastError();
@@ -40,7 +43,8 @@ int main()
         printf("socketError: %d\n", socketErrorCode);
 
         // stop usage of Winsock 2-DLL
-        int WSACleanupError = WSACleanup();
+        int WSACleanupError = 0;
+        WSACleanupError = WSACleanup();
         if (WSACleanupError == SOCKET_ERROR)
         {
             int WSACleanupErrorCode = WSAGetLastError();
@@ -50,10 +54,106 @@ int main()
         return 1;
     }
 
-    // bind socket:
+    // bind socket: (maybe change htons and inet_addr!)
+    struct sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr("127.0.0.1");
+    service.sin_port = htons(80);
+    int bindError = 0;
+    bindError = bind(sock, (SOCKADDR *)&service, sizeof(service));
+    if (bindError == SOCKET_ERROR)
+    {
+        int bindErrorCode = WSAGetLastError();
+        printf("bindError: %d\n", bindErrorCode);
+
+        // close sock socket
+        int closesocketError = 0;
+        closesocketError = closesocket(sock);
+        if (closesocketError == SOCKET_ERROR)
+        {
+            int closesocketErrorCode = WSAGetLastError();
+            printf("closesocketError: %d\n", closesocketErrorCode);
+        }
+
+        // stop usage of Winsock 2-DLL
+        int WSACleanupError = 0;
+        WSACleanupError = WSACleanup();
+        if (WSACleanupError == SOCKET_ERROR)
+        {
+            int WSACleanupErrorCode = WSAGetLastError();
+            printf("WSACleanupError: %d\n", WSACleanupErrorCode);
+        }
+
+        return 1;
+    }
+
+    // listen on sock socket
+    int listenError = 0;
+    listenError = listen(sock, SOMAXCONN);
+    if (listenError == SOCKET_ERROR)
+    {
+        int listenErrorCode = WSAGetLastError();
+        printf("listenError: %d\n", listenErrorCode);
+
+        // close sock socket
+        int closesocketError = 0;
+        closesocketError = closesocket(sock);
+        if (closesocketError == SOCKET_ERROR)
+        {
+            int closesocketErrorCode = WSAGetLastError();
+            printf("closesocketError: %d\n", closesocketErrorCode);
+        }
+
+        // stop usage of Winsock 2-DLL
+        int WSACleanupError = 0;
+        WSACleanupError = WSACleanup();
+        if (WSACleanupError == SOCKET_ERROR)
+        {
+            int WSACleanupErrorCode = WSAGetLastError();
+            printf("WSACleanupError: %d\n", WSACleanupErrorCode);
+        }
+
+        return 1;
+    }
+
+    SOCKET AcceptSocket;
+
+    while (1)
+    {
+        // accept client socket
+        AcceptSocket = accept(sock, NULL, NULL);
+        if (AcceptSocket == INVALID_SOCKET)
+        {
+            int acceptErrorCode = WSAGetLastError();
+            printf("acceptError: %d\n", acceptErrorCode);
+            continue;
+        }
+
+        // send data to client
+        int sendError = send(AcceptSocket, httpResponse, (int)strlen(httpResponse), 0);
+        if (sendError == SOCKET_ERROR)
+        {
+            int sendErrorCode = WSAGetLastError();
+            printf("sendError: %d\n", sendErrorCode);
+
+            // close AcceptSocket socket
+            int closesocketError = 0;
+            closesocketError = closesocket(AcceptSocket);
+            if (closesocketError == SOCKET_ERROR)
+            {
+                int closesocketErrorCode = WSAGetLastError();
+                printf("closesocketError: %d\n", closesocketErrorCode);
+            }
+
+            continue;
+        }
+
+        
+    }
 
     // stop usage of Winsock 2-DLL:
-    int WSACleanupError = WSACleanup();
+    int WSACleanupError = 0;
+    WSACleanupError = WSACleanup();
     if (WSACleanupError == SOCKET_ERROR)
     {
         int WSACleanupErrorCode = WSAGetLastError();
