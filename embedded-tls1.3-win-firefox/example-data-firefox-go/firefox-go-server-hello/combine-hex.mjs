32fe4267c8b6aa70c77d4dd42ccadf57de4f2749
Nico Hörter <nico.m.hoerter@gmail.com>
2024-08-04 11:47:05 +0200

    add script to seperate server hello message into different parts

--- embedded-tls1.3-win-firefox/example-data-firefox-go/firefox-go-server-hello/combine-hex.mjs
+++ embedded-tls1.3-win-firefox/example-data-firefox-go/firefox-go-server-hello/combine-hex.mjs
@@ -1,27 +1,41 @@
-import { readFile, writeFile, appendFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 
 const data1 = await readFile("1.bin", "hex");
 const data2 = await readFile("2.bin", "hex");
 const data3 = await readFile("3.bin", "hex");
 
-let hexContent = "";
+// make sure every server-hello dataset has the same length
+if (data1.length !== data2.length && data1.length !== data3.length) throw new Error("not every server-hello dataset has the same length!");
+
+/** @type {[string, string, string]} */
+const hexContent = ["", "", ""];
+let lastHexEqual = false;
 
 for (let i = 0; i < data1.length; i += 2) {
-    hexContent += `${data1[i]}${data1[i + 1]} `;
+    // compare current hex value:
+    if (`${data1[i]}${data1[i + 1]}` === `${data2[i]}${data2[i + 1]}` && `${data1[i]}${data1[i + 1]}` === `${data3[i]}${data3[i + 1]}`) {
+        if (!lastHexEqual) {
+            // from now on there is a new block of equal values
+            hexContent[0] += "------------ ";
+            hexContent[1] += "------------ ";
+            hexContent[2] += "------------ ";
+        }
+
+        lastHexEqual = true;
+    } else {
+        if (lastHexEqual) {
+            // from now on there is a new block of unequal values
+            hexContent[0] += ": ";
+            hexContent[1] += ": ";
+            hexContent[2] += ": ";
+        }
+
+        lastHexEqual = false;
+    }
+
+    hexContent[0] += `${data1[i]}${data1[i + 1]} `;
+    hexContent[1] += `${data2[i]}${data2[i + 1]} `;
+    hexContent[2] += `${data3[i]}${data3[i + 1]} `;
 }
 
-hexContent += "\r\n";
-
-for (let i = 0; i < data2.length; i += 2) {
-    hexContent += `${data2[i]}${data2[i + 1]} `;
-}
-
-hexContent += "\r\n";
-
-for (let i = 0; i < data3.length; i += 2) {
-    hexContent += `${data3[i]}${data3[i + 1]} `;
-}
-
-hexContent += "\r\n";
-
-await writeFile("1-3.txt", hexContent, "utf-8");
+await writeFile("1-3.txt", hexContent.join("\r\n") + "\r\n", "utf-8");
