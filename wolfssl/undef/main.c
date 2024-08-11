193a379451c64ae37b94517584d0e6d436f9bfc5
Nico Hörter <nico.m.hoerter@gmail.com>
2024-08-11 17:49:25 +0200

    undef test with linux

---
+++ wolfssl/undef/main.c
@@ -0,0 +1,7 @@
+#include "undef.h"
+// #include <stdio.h>
+
+int main() {
+    // printf("%ld\n", sizeof(long long));
+    return 0;
+}
---
+++ wolfssl/undef/undef.h
@@ -0,0 +1,369 @@
+// gcc -dM -E - < /dev/null
+
+// #undef __SSP_STRONG__ 3
+// #undef __DBL_MIN_EXP__ (-1021)
+// #undef __UINT_LEAST16_MAX__ 0xffff
+// #undef __ATOMIC_ACQUIRE 2
+// #undef __FLT128_MAX_10_EXP__ 4932
+// #undef __FLT_MIN__ 1.17549435082228750796873653722224568e-38F
+// #undef __GCC_IEC_559_COMPLEX 2
+// #undef __UINT_LEAST8_TYPE__ unsigned char
+// #undef __SIZEOF_FLOAT80__ 16
+// #undef __INTMAX_C(c) c ## L
+// #undef __CHAR_BIT__ 8
+// #undef __UINT8_MAX__ 0xff
+// #undef __SCHAR_WIDTH__ 8
+// #undef __WINT_MAX__ 0xffffffffU
+// #undef __FLT32_MIN_EXP__ (-125)
+// #undef __ORDER_LITTLE_ENDIAN__ 1234
+// #undef __SIZE_MAX__ 0xffffffffffffffffUL
+// #undef __WCHAR_MAX__ 0x7fffffff
+#undef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1
+#undef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2
+#undef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+// #undef __DBL_DENORM_MIN__ ((double)4.94065645841246544176568792868221372e-324L)
+#undef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+// #undef __GCC_ATOMIC_CHAR_LOCK_FREE 2
+// #undef __GCC_IEC_559 2
+// #undef __FLT32X_DECIMAL_DIG__ 17
+// #undef __FLT_EVAL_METHOD__ 0
+// #undef __FLT64_DECIMAL_DIG__ 17
+// #undef __CET__ 3
+// #undef __GCC_ATOMIC_CHAR32_T_LOCK_FREE 2
+// #undef __UINT_FAST64_MAX__ 0xffffffffffffffffUL
+// #undef __SIG_ATOMIC_TYPE__ int
+// #undef __DBL_MIN_10_EXP__ (-307)
+// #undef __FINITE_MATH_ONLY__ 0
+// #undef __FLT32X_MAX_EXP__ 1024
+#undef __FLT32_HAS_DENORM__
+// #undef __UINT_FAST8_MAX__ 0xff
+// #undef __FLT32_MAX_10_EXP__ 38
+// #undef __DEC64_MAX_EXP__ 385
+// #undef __INT8_C(c) c
+// #undef __INT_LEAST8_WIDTH__ 8
+// #undef __UINT_LEAST64_MAX__ 0xffffffffffffffffUL
+// #undef __SHRT_MAX__ 0x7fff
+// #undef __LDBL_MAX__ 1.18973149535723176502126385303097021e+4932L
+// #undef __FLT64X_MAX_10_EXP__ 4932
+// #undef __LDBL_IS_IEC_60559__ 2
+#undef __FLT64X_HAS_QUIET_NAN__
+// #undef __UINT_LEAST8_MAX__ 0xff
+// #undef __GCC_ATOMIC_BOOL_LOCK_FREE 2
+// #undef __FLT128_DENORM_MIN__ 6.47517511943802511092443895822764655e-4966F128
+// #undef __UINTMAX_TYPE__ long unsigned int
+#undef __linux
+// #undef __DEC32_EPSILON__ 1E-6DF
+// #undef __FLT_EVAL_METHOD_TS_18661_3__ 0
+#undef __unix
+// #undef __UINT32_MAX__ 0xffffffffU
+// #undef __FLT128_MIN_EXP__ (-16381)
+// #undef __WINT_MIN__ 0U
+// #undef __FLT128_MIN_10_EXP__ (-4931)
+// #undef __FLT32X_IS_IEC_60559__ 2
+// #undef __INT_LEAST16_WIDTH__ 16
+// #undef __SCHAR_MAX__ 0x7f
+// #undef __FLT128_MANT_DIG__ 113
+// #undef __WCHAR_MIN__ (-__WCHAR_MAX__ - 1)
+// #undef __INT64_C(c) c ## L
+// #undef __GCC_ATOMIC_POINTER_LOCK_FREE 2
+// #undef __FLT32X_MANT_DIG__ 53
+// #undef __USER_LABEL_PREFIX__ 
+// #undef __FLT64X_EPSILON__ 1.08420217248550443400745280086994171e-19F64x
+// #undef __STDC_HOSTED__
+// #undef __DEC64_MIN_EXP__ (-382)
+// #undef __DBL_DIG__ 15
+// #undef __FLT32_DIG__ 6
+// #undef __FLT_EPSILON__ 1.19209289550781250000000000000000000e-7F
+// #undef __SHRT_WIDTH__ 16
+// #undef __FLT32_IS_IEC_60559__ 2
+// #undef __LDBL_MIN__ 3.36210314311209350626267781732175260e-4932L
+// #undef __STDC_UTF_16__
+// #undef __DBL_IS_IEC_60559__ 2
+// #undef __DEC32_MAX__ 9.999999E96DF
+// #undef __FLT64X_DENORM_MIN__ 3.64519953188247460252840593361941982e-4951F64x
+#undef __FLT32X_HAS_INFINITY__
+// #undef __INT32_MAX__ 0x7fffffff
+#undef __unix__
+// #undef __INT_WIDTH__ 32
+// #undef __SIZEOF_LONG__ 8
+// #undef __STDC_IEC_559__
+// #undef __STDC_ISO_10646__ 201706L
+// #undef __UINT16_C(c) c
+// #undef __DECIMAL_DIG__ 21
+// #undef __STDC_IEC_559_COMPLEX__
+// #undef __FLT64_EPSILON__ 2.22044604925031308084726333618164062e-16F64
+#undef __gnu_linux__
+// #undef __FLT128_IS_IEC_60559__ 2
+// #undef __FLT64X_MIN_10_EXP__ (-4931)
+#undef __LDBL_HAS_QUIET_NAN__
+// #undef __FLT64_MANT_DIG__ 53
+// #undef __FLT64X_MANT_DIG__ 64
+// #undef __GNUC__ 11
+// #undef __pie__ 2
+#undef __MMX__
+#undef __FLT_HAS_DENORM__
+// #undef __SIZEOF_LONG_DOUBLE__ 16
+// #undef __BIGGEST_ALIGNMENT__ 16
+// #undef __FLT64_MAX_10_EXP__ 308
+// #undef __DBL_MAX__ ((double)1.79769313486231570814527423731704357e+308L)
+// #undef __INT_FAST32_MAX__ 0x7fffffffffffffffL
+#undef __DBL_HAS_INFINITY__
+// #undef __SIZEOF_FLOAT__ 4
+#undef __HAVE_SPECULATION_SAFE_VALUE
+// #undef __DEC32_MIN_EXP__ (-94)
+// #undef __INTPTR_WIDTH__ 64
+#undef __FLT64X_HAS_INFINITY__
+// #undef __UINT_LEAST32_MAX__ 0xffffffffU
+#undef __FLT32X_HAS_DENORM__
+// #undef __INT_FAST16_TYPE__ long int
+#undef __MMX_WITH_SSE__ 
+#undef __LDBL_HAS_DENORM__ 
+#undef __FLT128_HAS_INFINITY__ 
+// #undef __DEC32_MIN__ 1E-95DF
+// #undef __DBL_MAX_EXP__ 1024
+// #undef __WCHAR_WIDTH__ 32
+// #undef __FLT32_MAX__ 3.40282346638528859811704183484516925e+38F32
+// #undef __DEC128_EPSILON__ 1E-33DL
+#undef __SSE2_MATH__ 
+// #undef __ATOMIC_HLE_RELEASE 131072
+// #undef __PTRDIFF_MAX__ 0x7fffffffffffffffL
+#undef __amd64
+// #undef __ATOMIC_HLE_ACQUIRE 65536
+// #undef __LONG_LONG_MAX__ 0x7fffffffffffffffLL
+// #undef __SIZEOF_SIZE_T__ 8
+// #undef __FLT64X_MIN_EXP__ (-16381)
+// #undef __SIZEOF_WINT_T__ 4
+// #undef __LONG_LONG_WIDTH__ 64
+// #undef __FLT32_MAX_EXP__ 128
+// #undef __GXX_ABI_VERSION 1016
+// #undef __FLT_MIN_EXP__ (-125)
+#undef __GCC_HAVE_DWARF2_CFI_ASM 
+// #undef __INT16_MAX__ 0x7fff
+#undef __x86_64 
+// #undef __INT_FAST64_TYPE__ long int
+// #undef __FLT64_DENORM_MIN__ 4.94065645841246544176568792868221372e-324F64
+// #undef __DBL_MIN__ ((double)2.22507385850720138309023271733240406e-308L)
+// #undef __FLT128_EPSILON__ 1.92592994438723585305597794258492732e-34F128
+// #undef __FLT64X_NORM_MAX__ 1.18973149535723176502126385303097021e+4932F64x
+// #undef __SIZEOF_POINTER__ 8
+#undef __LP64__ 
+#undef __DBL_HAS_QUIET_NAN__ 
+// #undef __FLT32X_EPSILON__ 2.22044604925031308084726333618164062e-16F32x
+#undef __DECIMAL_BID_FORMAT__ 
+// #undef __FLT64_MIN_EXP__ (-1021)
+// #undef __FLT64_MIN_10_EXP__ (-307)
+// #undef __FLT64X_DECIMAL_DIG__ 21
+// #undef __DEC128_MIN__ 1E-6143DL
+#undef __REGISTER_PREFIX__ 
+// #undef __UINT16_MAX__ 0xffff
+#undef __DBL_HAS_DENORM__ 
+#undef __LDBL_HAS_INFINITY__ 
+// #undef __FLT32_MIN__ 1.17549435082228750796873653722224568e-38F32
+// #undef __UINT8_TYPE__ unsigned char
+// #undef __FLT_DIG__ 6
+#undef __NO_INLINE__ 
+// #undef __DEC_EVAL_METHOD__ 2
+// #undef __DEC128_MAX__ 9.999999999999999999999999999999999E6144DL
+// #undef __FLT_MANT_DIG__ 24
+// #undef __LDBL_DECIMAL_DIG__ 21
+// #undef __VERSION__ "11.4.0"
+// #undef __UINT64_C(c) c ## UL
+#undef _STDC_PREDEF_H 
+// #undef __INT_LEAST32_MAX__ 0x7fffffff
+// #undef __GCC_ATOMIC_INT_LOCK_FREE 2
+// #undef __FLT128_MAX_EXP__ 16384
+// #undef __FLT32_MANT_DIG__ 24
+// #undef __FLOAT_WORD_ORDER__ __ORDER_LITTLE_ENDIAN__
+// #undef __STDC_IEC_60559_COMPLEX__ 201404L
+#undef __FLT128_HAS_DENORM__ 
+// #undef __FLT32_DECIMAL_DIG__ 9
+// #undef __FLT128_DIG__ 33
+// #undef __INT32_C(c) c
+// #undef __DEC64_EPSILON__ 1E-15DD
+// #undef __ORDER_PDP_ENDIAN__ 3412
+// #undef __DEC128_MIN_EXP__ (-6142)
+// #undef __INT_FAST32_TYPE__ long int
+// #undef __UINT_LEAST16_TYPE__ short unsigned int
+#undef unix 
+// #undef __SIZE_TYPE__ long unsigned int
+// #undef __UINT64_MAX__ 0xffffffffffffffffUL
+// #undef __FLT_IS_IEC_60559__ 2
+// #undef __GNUC_WIDE_EXECUTION_CHARSET_NAME "UTF-32LE"
+// #undef __FLT64X_DIG__ 18
+// #undef __INT8_TYPE__ signed char
+#undef __ELF__ 
+#undef __GCC_ASM_FLAG_OUTPUTS__ 
+// #undef __UINT32_TYPE__ unsigned int
+// #undef __FLT_RADIX__ 2
+// #undef __INT_LEAST16_TYPE__ short int
+// #undef __LDBL_EPSILON__ 1.08420217248550443400745280086994171e-19L
+// #undef __UINTMAX_C(c) c ## UL
+#undef __SSE_MATH__ 
+#undef __k8 
+// #undef __FLT32X_MIN__ 2.22507385850720138309023271733240406e-308F32x
+// #undef __SIG_ATOMIC_MAX__ 0x7fffffff
+// #undef __GCC_ATOMIC_WCHAR_T_LOCK_FREE 2
+// #undef __STDC_IEC_60559_BFP__ 201404L
+// #undef __SIZEOF_PTRDIFF_T__ 8
+// #undef __LDBL_DIG__ 18
+// #undef __FLT64_IS_IEC_60559__ 2
+#undef __x86_64__ 
+// #undef __FLT32X_MIN_EXP__ (-1021)
+// #undef __DEC32_SUBNORMAL_MIN__ 0.000001E-95DF
+// #undef __INT_FAST16_MAX__ 0x7fffffffffffffffL
+// #undef __FLT64_DIG__ 15
+// #undef __UINT_FAST32_MAX__ 0xffffffffffffffffUL
+// #undef __UINT_LEAST64_TYPE__ long unsigned int
+#undef __FLT_HAS_QUIET_NAN__ 
+// #undef __FLT_MAX_10_EXP__ 38
+// #undef __LONG_MAX__ 0x7fffffffffffffffL
+#undef __FLT64X_HAS_DENORM__ 
+// #undef __DEC128_SUBNORMAL_MIN__ 0.000000000000000000000000000000001E-6143DL
+#undef __FLT_HAS_INFINITY__ 
+// #undef __GNUC_EXECUTION_CHARSET_NAME "UTF-8"
+// #undef __UINT_FAST16_TYPE__ long unsigned int
+// #undef __DEC64_MAX__ 9.999999999999999E384DD
+// #undef __INT_FAST32_WIDTH__ 64
+// #undef __CHAR16_TYPE__ short unsigned int
+#undef __PRAGMA_REDEFINE_EXTNAME 
+// #undef __SIZE_WIDTH__ 64
+#undef __SEG_FS 
+// #undef __INT_LEAST16_MAX__ 0x7fff
+// #undef __DEC64_MANT_DIG__ 16
+// #undef __INT64_MAX__ 0x7fffffffffffffffL
+#undef __SEG_GS 
+// #undef __FLT32_DENORM_MIN__ 1.40129846432481707092372958328991613e-45F32
+// #undef __SIG_ATOMIC_WIDTH__ 32
+// #undef __INT_LEAST64_TYPE__ long int
+// #undef __INT16_TYPE__ short int
+// #undef __INT_LEAST8_TYPE__ signed char
+// #undef __STDC_VERSION__ 201710L
+// #undef __SIZEOF_INT__ 4
+// #undef __DEC32_MAX_EXP__ 97
+// #undef __INT_FAST8_MAX__ 0x7f
+// #undef __FLT128_MAX__ 1.18973149535723176508575932662800702e+4932F128
+// #undef __INTPTR_MAX__ 0x7fffffffffffffffL
+#undef linux 
+#undef __FLT64_HAS_QUIET_NAN__ 
+// #undef __FLT32_MIN_10_EXP__ (-37)
+// #undef __FLT32X_DIG__ 15
+// #undef __PTRDIFF_WIDTH__ 64
+// #undef __LDBL_MANT_DIG__ 64
+#undef __FLT64_HAS_INFINITY__ 
+// #undef __FLT64X_MAX__ 1.18973149535723176502126385303097021e+4932F64x
+// #undef __SIG_ATOMIC_MIN__ (-__SIG_ATOMIC_MAX__ - 1)
+#undef __code_model_small__ 
+// #undef __GCC_ATOMIC_LONG_LOCK_FREE 2
+// #undef __DEC32_MANT_DIG__ 7
+#undef __k8__ 
+// #undef __INTPTR_TYPE__ long int
+// #undef __UINT16_TYPE__ short unsigned int
+// #undef __WCHAR_TYPE__ int
+// #undef __pic__ 2
+// #undef __UINTPTR_MAX__ 0xffffffffffffffffUL
+// #undef __INT_FAST64_WIDTH__ 64
+// #undef __INT_FAST64_MAX__ 0x7fffffffffffffffL
+#undef __GCC_ATOMIC_TEST_AND_SET_TRUEVAL 
+// #undef __FLT_NORM_MAX__ 3.40282346638528859811704183484516925e+38F
+#undef __FLT32_HAS_INFINITY__ 
+// #undef __FLT64X_MAX_EXP__ 16384
+// #undef __UINT_FAST64_TYPE__ long unsigned int
+// #undef __INT_MAX__ 0x7fffffff
+#undef __linux__ 
+// #undef __INT64_TYPE__ long int
+// #undef __FLT_MAX_EXP__ 128
+// #undef __ORDER_BIG_ENDIAN__ 4321
+// #undef __DBL_MANT_DIG__ 53
+// #undef __SIZEOF_FLOAT128__ 16
+// #undef __INT_LEAST64_MAX__ 0x7fffffffffffffffL
+// #undef __GCC_ATOMIC_CHAR16_T_LOCK_FREE 2
+// #undef __DEC64_MIN__ 1E-383DD
+// #undef __WINT_TYPE__ unsigned int
+// #undef __UINT_LEAST32_TYPE__ unsigned int
+// #undef __SIZEOF_SHORT__ 2
+// #undef __FLT32_NORM_MAX__ 3.40282346638528859811704183484516925e+38F32
+#undef __SSE__ 
+// #undef __LDBL_MIN_EXP__ (-16381)
+// #undef __FLT64_MAX__ 1.79769313486231570814527423731704357e+308F64
+#undef __amd64__ 
+// #undef __WINT_WIDTH__ 32
+// #undef __INT_LEAST8_MAX__ 0x7f
+// #undef __INT_LEAST64_WIDTH__ 64
+// #undef __LDBL_MAX_EXP__ 16384
+// #undef __FLT32X_MAX_10_EXP__ 308
+// #undef __SIZEOF_INT128__ 16
+// #undef __FLT64X_IS_IEC_60559__ 2
+// #undef __LDBL_MAX_10_EXP__ 4932
+#undef __ATOMIC_RELAXED 
+// #undef __DBL_EPSILON__ ((double)2.22044604925031308084726333618164062e-16L)
+// #undef __FLT128_MIN__ 3.36210314311209350626267781732175260e-4932F128
+#undef _LP64 
+// #undef __UINT8_C(c) c
+// #undef __FLT64_MAX_EXP__ 1024
+// #undef __INT_LEAST32_TYPE__ int
+// #undef __SIZEOF_WCHAR_T__ 4
+// #undef __UINT64_TYPE__ long unsigned int
+#undef __GNUC_PATCHLEVEL__ 
+// #undef __FLT128_NORM_MAX__ 1.18973149535723176508575932662800702e+4932F128
+// #undef __FLT64_NORM_MAX__ 1.79769313486231570814527423731704357e+308F64
+#undef __FLT128_HAS_QUIET_NAN__ 
+// #undef __INTMAX_MAX__ 0x7fffffffffffffffL
+// #undef __INT_FAST8_TYPE__ signed char
+// #undef __FLT64X_MIN__ 3.36210314311209350626267781732175260e-4932F64x
+#undef __GNUC_STDC_INLINE__ 
+#undef __FLT64_HAS_DENORM__ 
+// #undef __FLT32_EPSILON__ 1.19209289550781250000000000000000000e-7F32
+// #undef __DBL_DECIMAL_DIG__ 17
+//#undef __STDC_UTF_32__ 
+// #undef __INT_FAST8_WIDTH__ 8
+#undef __FXSR__ 
+// #undef __FLT32X_MAX__ 1.79769313486231570814527423731704357e+308F32x
+// #undef __DBL_NORM_MAX__ ((double)1.79769313486231570814527423731704357e+308L)
+// #undef __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
+// #undef __INTMAX_WIDTH__ 64
+// #undef __UINT32_C(c) c ## U
+// #undef __FLT_DENORM_MIN__ 1.40129846432481707092372958328991613e-45F
+// #undef __INT8_MAX__ 0x7f
+// #undef __LONG_WIDTH__ 64
+// #undef __PIC__ 2
+// #undef __UINT_FAST32_TYPE__ long unsigned int
+// #undef __FLT32X_NORM_MAX__ 1.79769313486231570814527423731704357e+308F32x
+// #undef __CHAR32_TYPE__ unsigned int
+// #undef __FLT_MAX__ 3.40282346638528859811704183484516925e+38F
+#undef __SSE2__ 
+// #undef __INT32_TYPE__ int
+#undef __SIZEOF_DOUBLE__ 
+// #undef __FLT_MIN_10_EXP__ (-37)
+// #undef __FLT64_MIN__ 2.22507385850720138309023271733240406e-308F64
+// #undef __INT_LEAST32_WIDTH__ 32
+// #undef __INTMAX_TYPE__ long int
+// #undef __DEC128_MAX_EXP__ 6145
+#undef __FLT32X_HAS_QUIET_NAN__ 
+#undef __ATOMIC_CONSUME 
+// #undef __GNUC_MINOR__ 4
+// #undef __INT_FAST16_WIDTH__ 64
+// #undef __UINTMAX_MAX__ 0xffffffffffffffffUL
+// #undef __PIE__ 2
+// #undef __FLT32X_DENORM_MIN__ 4.94065645841246544176568792868221372e-324F32x
+// #undef __DBL_MAX_10_EXP__ 308
+// #undef __LDBL_DENORM_MIN__ 3.64519953188247460252840593361941982e-4951L
+// #undef __INT16_C(c) c
+//#undef __STDC__ 
+// #undef __PTRDIFF_TYPE__ long int
+// #undef __ATOMIC_SEQ_CST 5
+// #undef __FLT32X_MIN_10_EXP__ (-307)
+// #undef __UINTPTR_TYPE__ long unsigned int
+// #undef __DEC64_SUBNORMAL_MIN__ 0.000000000000001E-383DD
+// #undef __DEC128_MANT_DIG__ 34
+// #undef __LDBL_MIN_10_EXP__ (-4931)
+// #undef __SIZEOF_LONG_LONG__ 8
+// #undef __FLT128_DECIMAL_DIG__ 36
+// #undef __GCC_ATOMIC_LLONG_LOCK_FREE 2
+#undef __FLT32_HAS_QUIET_NAN__ 
+// #undef __FLT_DECIMAL_DIG__ 9
+// #undef __UINT_FAST16_MAX__ 0xffffffffffffffffUL
+// #undef __LDBL_NORM_MAX__ 1.18973149535723176502126385303097021e+4932L
+// #undef __GCC_ATOMIC_SHORT_LOCK_FREE 2
+// #undef __UINT_FAST8_TYPE__ unsigned char
+// #undef __ATOMIC_ACQ_REL 4
+// #undef __ATOMIC_RELEASE 3
